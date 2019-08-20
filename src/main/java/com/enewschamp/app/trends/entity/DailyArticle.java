42646a6f6342b7666edda5cbbcbae78455ea22d5
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-08-21 00:29:51 +0530

    Update DailyArticle.java

--- src/main/java/com/enewschamp/app/trends/entity/DailyArticle.java
+++ src/main/java/com/enewschamp/app/trends/entity/DailyArticle.java
@@ -19,7 +19,7 @@ public class DailyArticle {
 
 	@Id
 	@Column(name = "publishDate", updatable = false, nullable = false)
-	private Integer publishDate;
+	private LocalDate publishDate;
 	
 	@Column(name = "articlesRead")
 	private Long articlesRead;
