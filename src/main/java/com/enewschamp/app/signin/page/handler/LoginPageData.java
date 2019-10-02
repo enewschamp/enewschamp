3fc51911470e52f5011c9729a655b3fa9e9ea191
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-02 22:04:11 +0530

    Student Login

--- src/main/java/com/enewschamp/app/signin/page/handler/LoginPageData.java
+++ src/main/java/com/enewschamp/app/signin/page/handler/LoginPageData.java
@@ -1,9 +1,11 @@
 package com.enewschamp.app.signin.page.handler;
 
+import com.enewschamp.app.common.PageData;
+
 import lombok.Data;
 
 @Data
-public class LoginPageData implements java.io.Serializable{
+public class LoginPageData extends PageData implements java.io.Serializable{
 
 	/**
 	 * 
@@ -11,5 +13,7 @@ public class LoginPageData implements java.io.Serializable{
 	private static final long serialVersionUID = 1L;
 	private String emailId;
 	private String password;
+	private String deviceId;
+	private String message;
 	
 }
