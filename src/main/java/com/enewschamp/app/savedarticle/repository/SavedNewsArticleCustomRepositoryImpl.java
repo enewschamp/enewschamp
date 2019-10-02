4c41adb126c1dc1c25fa3de749c10f9afacab4de
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-02 16:18:09 +0530

    issue fix 21 Sep feedback

--- src/main/java/com/enewschamp/app/savedarticle/repository/SavedNewsArticleCustomRepositoryImpl.java
+++ src/main/java/com/enewschamp/app/savedarticle/repository/SavedNewsArticleCustomRepositoryImpl.java
@@ -80,4 +80,57 @@ public class SavedNewsArticleCustomRepositoryImpl extends RepositoryImpl impleme
 
 		return new PageImpl<>(list, pageable, count);
 	}
+	@Override
+	public Page<SavedNewsArticleSummaryDTO> findArticlesWithOpinions(SavedNewsArticleSearchRequest searchRequest,
+			Pageable pageable) {
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<SavedNewsArticleSummaryDTO> criteriaQuery = cb.createQuery(SavedNewsArticleSummaryDTO.class);
+		Root<NewsArticleGroup> articleGroupRoot = criteriaQuery.from(NewsArticleGroup.class);
+		Root<NewsArticle> articleRoot = criteriaQuery.from(NewsArticle.class);
+		Root<StudentActivity> studentActivityRoot = criteriaQuery.from(StudentActivity.class);
+
+		criteriaQuery = criteriaQuery.select(cb.construct(SavedNewsArticleSummaryDTO.class, articleRoot.get("newsArticleId"),
+				 articleRoot.get("publishDate"), 
+				articleGroupRoot.get("genreId"), articleGroupRoot.get("headline"), studentActivityRoot.get("opinion")));
+
+		// Build filter conditions
+		List<Predicate> filterPredicates = new ArrayList<>();
+
+		filterPredicates
+				.add(cb.equal(articleGroupRoot.get("newsArticleGroupId"), articleRoot.get("newsArticleGroupId")));
+		filterPredicates.add(cb.equal(studentActivityRoot.get("newsArticleId"), articleRoot.get("newsArticleId")));
+		filterPredicates.add(cb.notEqual(studentActivityRoot.get("opinion"), ""));
+
+		if (searchRequest.getPublicationDate() != null) {
+			filterPredicates.add(cb.equal(articleRoot.get("publishDate"), searchRequest.getPublicationDate()));
+		}
+
+		if (searchRequest.getGenreId() != null) {
+			filterPredicates.add(cb.equal(articleGroupRoot.get("genreId"), searchRequest.getGenreId()));
+		}
+		if (searchRequest.getEditionId() != null) {
+			filterPredicates.add(cb.equal(articleGroupRoot.get("editionId"), searchRequest.getEditionId()));
+		}
+		if (searchRequest.getHeadline() != null) {
+			filterPredicates.add(cb.equal(articleGroupRoot.get("headline"), searchRequest.getHeadline()));
+		}
+
+		criteriaQuery.where(cb.and((Predicate[]) filterPredicates.toArray(new Predicate[0])));
+		criteriaQuery.orderBy(cb.desc(articleRoot.get("operationDateTime")));
+		
+		// Build query
+		TypedQuery<SavedNewsArticleSummaryDTO> q = entityManager.createQuery(criteriaQuery);
+
+		if (pageable.getPageSize() > 0) {
+			int pageNumber = pageable.getPageNumber();
+			q.setFirstResult(pageNumber * pageable.getPageSize());
+			q.setMaxResults(pageable.getPageSize());
+
+		}
+		List<SavedNewsArticleSummaryDTO> list = q.getResultList();
+		long count = getRecordCount(criteriaQuery, filterPredicates, articleRoot);
+
+		return new PageImpl<>(list, pageable, count);
+	}
 }
