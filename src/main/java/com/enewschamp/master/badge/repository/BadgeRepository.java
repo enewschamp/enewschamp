3349b5a5ee411fb1514e5ce2f618097c44d2ce32
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-06 18:48:27 +0530

    added getNextBadgeForGenre and getNextBadge
    for welcome page

--- src/main/java/com/enewschamp/master/badge/repository/BadgeRepository.java
+++ src/main/java/com/enewschamp/master/badge/repository/BadgeRepository.java
@@ -1,8 +1,8 @@
 package com.enewschamp.master.badge.repository;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,4 +20,9 @@ public interface BadgeRepository extends JpaRepository<Badge, String>{
 	@Query("select d from Badge d where d.editionId= :editionId and d.genreId= :genreId and d.recordInUse ='Y' and d.monthlyPointsToScore <= :monthlyPointsToScore ")
 	public Optional<Badge> getBadgeForStudent( @Param("editionId") String editionId, @Param("genreId")  String genreId, @Param("monthlyPointsToScore") Long studentpoints);
 	
+	@Query("select d from Badge d where d.editionId= :editionId and d.recordInUse ='Y' and d.monthlyPointsToScore > :studentpoints order by d.monthlyPointsToScore")
+	public List<Badge> getNextBadge( @Param("editionId") String editionId, @Param("studentpoints") Long studentpoints);
+
+	@Query("select d from Badge d where d.editionId= :editionId and d.recordInUse ='Y' and d.genreId= :genreId and d.monthlyPointsToScore > :studentpoints order by d.monthlyPointsToScore")
+	public List<Badge> getNextBadgeForGenre( @Param("editionId") String editionId,@Param("genreId")  String genreId, @Param("studentpoints") Long studentpoints);
 }
