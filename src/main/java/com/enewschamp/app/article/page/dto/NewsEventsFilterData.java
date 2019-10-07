631ecfc9d6d3488b609531a98c8bfff0bae92c10
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-07 13:11:18 +0530

    News and events

---
+++ src/main/java/com/enewschamp/app/article/page/dto/NewsEventsFilterData.java
@@ -0,0 +1,12 @@
+package com.enewschamp.app.article.page.dto;
+
+import lombok.Data;
+
+@Data
+public class NewsEventsFilterData {
+
+	private String cityID;
+	private String month;
+	private String headline;
+	
+}
