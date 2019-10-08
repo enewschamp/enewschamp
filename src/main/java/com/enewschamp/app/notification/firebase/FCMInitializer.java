48761192600fa3ac6e330573a82eaa3c8d2de752
mesmayus <mesmayus@gmail.com>
2019-10-08 15:34:55 +0530

    Firebase integration code

---
+++ src/main/java/com/enewschamp/app/notification/firebase/FCMInitializer.java
@@ -0,0 +1,39 @@
+package com.enewschamp.app.notification.firebase;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import lombok.extern.java.Log;
+
+@Service
+@Log
+public class FCMInitializer {
+
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
+
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())).build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("Firebase application has been initialized");
+            }
+        } catch (IOException e) {
+            log.severe(e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/com/enewschamp/app/notification/firebase/controller/PushNotificationController.java
@@ -0,0 +1,46 @@
+package com.enewschamp.app.notification.firebase.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enewschamp.app.notification.firebase.model.PushNotificationRequest;
+import com.enewschamp.app.notification.firebase.model.PushNotificationResponse;
+import com.enewschamp.app.notification.firebase.service.PushNotificationService;
+
+@RestController
+public class PushNotificationController {
+
+    private PushNotificationService pushNotificationService;
+
+    public PushNotificationController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+
+    @PostMapping("/notification/topic")
+    public ResponseEntity<PushNotificationResponse> sendNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/token")
+    public ResponseEntity<PushNotificationResponse> sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/data")
+    public ResponseEntity<PushNotificationResponse> sendDataNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotification(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @GetMapping("/notification")
+    public ResponseEntity<PushNotificationResponse> sendSampleNotification() {
+        pushNotificationService.sendSamplePushNotification();
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+}
