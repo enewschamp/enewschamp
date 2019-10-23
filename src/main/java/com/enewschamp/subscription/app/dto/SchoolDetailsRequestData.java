cf81a978404d465426aaa0779ba110987ba52ff6
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-23 16:40:24 +0530

    new class

---
+++ src/main/java/com/enewschamp/subscription/app/dto/SchoolDetailsRequestData.java
@@ -0,0 +1,11 @@
+package com.enewschamp.subscription.app.dto;
+
+import lombok.Data;
+
+@Data
+public class SchoolDetailsRequestData {
+
+	private String countryID;
+	private String stateID;
+	private String cityID;
+}
