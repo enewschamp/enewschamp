730238af378684e51832a450317ac1bb3cd03d74
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-23 15:53:18 +0530

    added security filter

---
+++ src/main/java/com/enewschamp/security/service/AppSecurityService.java
@@ -0,0 +1,74 @@
+package com.enewschamp.app.security.service;
+
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.enewschamp.app.common.ErrorCodes;
+import com.enewschamp.app.security.entity.AppSecurity;
+import com.enewschamp.app.security.repository.AppSecurityRepository;
+import com.enewschamp.problem.BusinessException;
+
+@Service
+public class AppSecurityService {
+
+	@Autowired
+	AppSecurityRepository appSecurityRepository;
+	
+	@Autowired
+	ModelMapper modelMapper;
+	
+	@Autowired
+	@Qualifier("modelPatcher")
+	ModelMapper modelMapperForPatch;
+	
+	public AppSecurity create(AppSecurity appSecurityEntity) {
+		return appSecurityRepository.save(appSecurityEntity);
+	}
+
+	public AppSecurity update(AppSecurity appSecurityEntity) {
+		Long appSecurityId = appSecurityEntity.getAppSecId();
+		AppSecurity existingAppSecurity = get(appSecurityId);
+		modelMapper.map(appSecurityEntity, existingAppSecurity);
+		return appSecurityRepository.save(existingAppSecurity);
+	}
+
+	public AppSecurity patch(AppSecurity appSecurity) {
+		Long appSecurityId = appSecurity.getAppSecId();
+		AppSecurity existingEntity = get(appSecurityId);
+		modelMapperForPatch.map(appSecurity, existingEntity);
+		return appSecurityRepository.save(existingEntity);
+	}
+
+	public void delete(Long appSecurityId) {
+		appSecurityRepository.deleteById(appSecurityId);
+	}
+
+	public AppSecurity get(Long appSecurityId) {
+		Optional<AppSecurity> existingEntity = appSecurityRepository.findById(appSecurityId);
+		if (existingEntity.isPresent()) {
+			return existingEntity.get();
+		} else {
+			throw new BusinessException(ErrorCodes.APP_SEC_KEY_NOT_FOUND);
+		}
+	}
+	
+
+	@Cacheable("appSec")
+	public boolean isValidKey(final String appName, final String appKey)
+	{
+		Optional<AppSecurity> existingEntity = appSecurityRepository.getAppSec(appName,appKey);
+		if(existingEntity.isPresent())
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
