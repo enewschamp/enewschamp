8f8c68c4dbecda0e779c68e439bc35fcaa186846
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-23 15:59:13 +0530

    added login service

---
+++ src/main/java/com/enewschamp/app/user/login/entity/UserLogin.java
@@ -0,0 +1,57 @@
+package com.enewschamp.app.user.login.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.enewschamp.domain.common.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "UserLogin")
+@EqualsAndHashCode(callSuper = false)
+public class UserLogin extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userLogin_id_generator")
+	@SequenceGenerator(name = "userLogin_id_generator", sequenceName = "userLogin_id_seq", allocationSize = 1)
+	@Column(name = "userLoginId", updatable = false, nullable = false)
+	private Long userLoginId;
+
+	
+	@NotNull
+	@Column(name = "userId", length = 80)
+	private String userId;
+
+	
+	@Column(name = "loginFlag", length = 1)
+	private String loginFlag;
+
+	@Column(name = "status", length = 1)
+	private String status;
+
+	@Column(name = "lastLoginTime")
+	private LocalDateTime lastLoginTime;
+	
+	@Column(name = "deviceId", length=50)
+	private String deviceId;
+	
+	@Column(name = "userType")
+	private UserType userType;
+
+}
