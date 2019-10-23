4edbcce49be77490ade8c8533153fe3429db7196
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-23 16:45:35 +0530

    new class

---
+++ src/main/java/com/enewschamp/subscription/app/dto/StateLOVDTO.java
@@ -0,0 +1,21 @@
+package com.enewschamp.subscription.app.dto;
+
+import java.util.List;
+
+import com.enewschamp.app.common.PageData;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class StateLOVDTO extends PageData{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String countryID;
+	private List<StatePageData> stateLOV;
+	
+}
