4a3c5eed1ab93308b448ae1c551dc547dff9d29d
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-23 16:30:39 +0530

    new method to get the state

--- src/main/java/com/enewschamp/app/common/state/repository/StateRepository.java
+++ src/main/java/com/enewschamp/app/common/state/repository/StateRepository.java
@@ -1,6 +1,7 @@
 package com.enewschamp.app.common.state.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,7 @@ public interface StateRepository extends JpaRepository<State, Long>{
 
 	@Query("Select s from State s where s.countryId= :countryId and s.recordInUse = 'Y' ")
 	public List<State> getStateForCountry(@Param("countryId" )String countryId);
+	
+	@Query("Select s from State s where s.nameId= :nameId and s.recordInUse = 'Y' ")
+	public Optional<State> getState(@Param("nameId" )String nameId);
 }
