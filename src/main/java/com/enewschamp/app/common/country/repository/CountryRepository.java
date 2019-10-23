751d6efe33dcd93d2b817352882e8da7e6eb1636
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-23 16:28:10 +0530

    new method to get country

--- src/main/java/com/enewschamp/app/common/country/repository/CountryRepository.java
+++ src/main/java/com/enewschamp/app/common/country/repository/CountryRepository.java
@@ -1,9 +1,11 @@
 package com.enewschamp.app.common.country.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.enewschamp.app.common.country.entity.Country;
 
@@ -11,4 +13,10 @@ public interface CountryRepository extends JpaRepository<Country, Long>{
 	
 	@Query("Select c from Country c")
 	public List<Country> getAll();
+	
+	@Query("Select c from Country c where c.recordInUse='Y'")
+	public List<Country> getCountries();
+	
+	@Query("Select c from Country c where c.recordInUse='Y' and  c.nameId= :nameId")
+	public Optional<Country> getCountry(@Param("nameId")String nameId);
 }
