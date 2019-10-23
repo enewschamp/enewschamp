e81d2ce1b3bf884032aa2fa05f9b697d911f9408
deepaksemwalgithub <50263416+deepaksemwalgithub@users.noreply.github.com>
2019-10-23 16:06:05 +0530

    added a check for secured access of pages

--- src/main/java/com/enewschamp/subscription/app/fw/page/controller/AppPageController.java
+++ src/main/java/com/enewschamp/subscription/app/fw/page/controller/AppPageController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.enewschamp.EnewschampApplicationProperties;
+import com.enewschamp.app.common.ErrorCodes;
 import com.enewschamp.app.common.HeaderDTO;
 import com.enewschamp.app.common.PageDTO;
 import com.enewschamp.app.common.PageRequestDTO;
@@ -21,9 +22,14 @@ import com.enewschamp.app.common.uicontrols.service.UIControlsService;
 import com.enewschamp.app.fw.page.navigation.common.PageAction;
 import com.enewschamp.app.fw.page.navigation.dto.PageNavigatorDTO;
 import com.enewschamp.app.fw.page.navigation.service.PageNavigationService;
+import com.enewschamp.app.user.login.entity.UserType;
+import com.enewschamp.app.user.login.service.UserLoginBusiness;
+import com.enewschamp.domain.common.AppConstants;
 import com.enewschamp.domain.common.PageHandlerFactory;
 import com.enewschamp.domain.common.PageNavigationContext;
+import com.enewschamp.problem.BusinessException;
 import com.enewschamp.publication.domain.service.EditionService;
+import com.enewschamp.subscription.domain.business.SubscriptionBusiness;
 
 import lombok.extern.java.Log;
 
@@ -32,131 +38,175 @@ import lombok.extern.java.Log;
 @RequestMapping("/enewschamp/ui/v1")
 public class AppPageController {
 
-
 	@Autowired
 	private PageHandlerFactory pageHandlerFactory;
-	
+
 	@Autowired
 	ModelMapper modelMapper;
-	
+
 	@Autowired
 	UIControlsService uiControlService;
-	
+
 	@Autowired
 	PageNavigationService pageNavigationService;
-	
+	@Autowired
+	SubscriptionBusiness subscriptionBusiness;
+
 	@Autowired
 	EditionService editionService;
 	@Autowired
 	EnewschampApplicationProperties appConfig;
 
-	
+	@Autowired
+	UserLoginBusiness studentLoginBusiness;
+
 	@PostMapping(value = "/app")
 	public ResponseEntity<PageDTO> processAppRequest(@RequestBody PageRequestDTO pageRequest) {
-		
+
 		String pageName = pageRequest.getHeader().getPageName();
 		String actionName = pageRequest.getHeader().getAction();
 		pageRequest.getHeader().setPageName(pageName);
 		pageRequest.getHeader().setAction(actionName);
-		
-		PageDTO pageResponse = processRequest(pageName, actionName, pageRequest,"app");
-		
+
+		PageDTO pageResponse = processRequest(pageName, actionName, pageRequest, "app");
+
 		return new ResponseEntity<PageDTO>(pageResponse, HttpStatus.OK);
 	}
-	
+
 	private PageDTO processRequest(String pageName, String actionName, PageRequestDTO pageRequest, String context) {
 		String operation = pageRequest.getHeader().getOperation();
 		String edition = pageRequest.getHeader().getEditionID();
-		//check if the edition exist..
+		// check if the edition exist..
 		editionService.getEdition(edition);
-		
-		//set default page size from app..
-		pageRequest.getHeader().setPageSize(appConfig.getPageSize());
-		
-		//get  the page navigation based for current page
-		PageNavigatorDTO pageNavDto = pageNavigationService.getNavPage(actionName, operation, pageName);
-		
-		//Process current page
-		//PageDTO pageResponse = pageHandlerFactory.getPageHandler(pageName).handleAppAction(actionName, pageRequest,pageNavDto);
-		PageDTO pageResponse = pageHandlerFactory.getPageHandler(pageName, context).handleAppAction(actionName, pageRequest,pageNavDto);
 
-		//save data in master Tables (including the previous unsaved data
-		String commitMasterData = pageNavDto.getCommitMasterData();
-		if("Y".equals(commitMasterData))
-		{
-			List<PageNavigatorDTO> pagenNavList =  pageNavigationService.getNavList(actionName, operation, pageName);
-			for(PageNavigatorDTO item:pagenNavList)
-			{
-				pageHandlerFactory.getPageHandler(item.getCurrentPage(),context).saveAsMaster(actionName, pageRequest);
+		// set default page size from app..
+		pageRequest.getHeader().setPageSize(appConfig.getPageSize());
+
+		// get the page navigation based for current page
+		PageNavigatorDTO pageNavDto = pageNavigationService.getNavPage(actionName, operation, pageName);
+
+		// check of the page page has secured access....and if the student subscription
+		// is valid...
+		if (pageNavDto != null && AppConstants.YES.toString().equals(pageNavDto.getSecured())) {
+			// validate of the user is already logged in..
+			String emailId = pageRequest.getHeader().getEmailID();
+			String deviceId = pageRequest.getHeader().getDeviceId();
+
+			if ("".equals(emailId) || null == emailId) {
+				throw new BusinessException(ErrorCodes.INVALID_EMAIL_ID, "Invalid Email Id");
+
+			} else {
+				boolean isLogged = studentLoginBusiness.isUserLoggedIn(deviceId, emailId, UserType.S);
+
+				if (!isLogged) {
+					boolean isSubscriptiionValid = subscriptionBusiness.isStudentSubscriptionValid(emailId, edition);
+					if (!isSubscriptiionValid)
+						throw new BusinessException(ErrorCodes.UNAUTH_ACCESS, "UnAuthorised Access");
+
+				}
 			}
 		}
-		String nextPageName="";
-		//Load next page.. If action is next load the next page.. If action is previous load the previous page.
-		if(PageAction.next.toString().equalsIgnoreCase(actionName) || PageAction.save.toString().equalsIgnoreCase(actionName) || PageAction.home.toString().equalsIgnoreCase(actionName) || PageAction.clickArticleImage.toString().equalsIgnoreCase(actionName) || PageAction.savedarticles.toString().equalsIgnoreCase(actionName) || PageAction.GoPremium.toString().equalsIgnoreCase(actionName) || PageAction.PicImage.toString().equalsIgnoreCase(actionName) || PageAction.opinions.toString().equalsIgnoreCase(actionName) || PageAction.subscription.toString().equalsIgnoreCase(actionName) || PageAction.savednewsarticle.toString().equalsIgnoreCase(actionName) || PageAction.MyActivity.toString().equalsIgnoreCase(actionName) || PageAction.Champs.toString().equalsIgnoreCase(actionName) || PageAction.Menu.toString().equalsIgnoreCase(actionName) || PageAction.HelpDesk.toString().equalsIgnoreCase(actionName) 
-				|| PageAction.Month.toString().equalsIgnoreCase(actionName) || PageAction.Year.toString().equalsIgnoreCase(actionName) || PageAction.Cancel.toString().equalsIgnoreCase(actionName) || PageAction.MyProfile.toString().equalsIgnoreCase(actionName) || PageAction.FilterSavedArticles.toString().equalsIgnoreCase(actionName) || PageAction.ClearFilterSavedArticles.toString().equalsIgnoreCase(actionName)  || PageAction.Scores.toString().equalsIgnoreCase(actionName) 
-				|| PageAction.Trends.toString().equalsIgnoreCase(actionName)|| PageAction.recognitions.toString().equalsIgnoreCase(actionName) || PageAction.LeftSwipe.toString().equalsIgnoreCase(actionName) || PageAction.RightSwipe.toString().equalsIgnoreCase(actionName) || PageAction.schooldetails.toString().equalsIgnoreCase(actionName) ||  PageAction.shareachievements.toString().equalsIgnoreCase(actionName) ||  PageAction.WelcomeUser.toString().equalsIgnoreCase(actionName) ||  PageAction.ClickNewsEvent.toString().equalsIgnoreCase(actionName) || PageAction.ClickOpinionArticle.toString().equalsIgnoreCase(actionName))
-		{
-			nextPageName = pageNavDto.getNextpage();
+		// Process current page
+		// PageDTO pageResponse =
+		// pageHandlerFactory.getPageHandler(pageName).handleAppAction(actionName,
+		// pageRequest,pageNavDto);
+		PageDTO pageResponse = pageHandlerFactory.getPageHandler(pageName, context).handleAppAction(actionName,
+				pageRequest, pageNavDto);
+
+		// save data in master Tables (including the previous unsaved data
+		String commitMasterData = pageNavDto.getCommitMasterData();
+		if ("Y".equals(commitMasterData)) {
+			List<PageNavigatorDTO> pagenNavList = pageNavigationService.getNavList(actionName, operation, pageName);
+			for (PageNavigatorDTO item : pagenNavList) {
+				pageHandlerFactory.getPageHandler(item.getCurrentPage(), context).saveAsMaster(actionName, pageRequest);
+			}
 		}
-		else if(PageAction.previous.toString().equalsIgnoreCase(actionName) || PageAction.back.toString().equalsIgnoreCase(actionName))
-		{
+		String nextPageName = "";
+		// Load next page.. If action is next load the next page.. If action is previous
+		// load the previous page.
+		if (PageAction.next.toString().equalsIgnoreCase(actionName)
+				|| PageAction.save.toString().equalsIgnoreCase(actionName)
+				|| PageAction.home.toString().equalsIgnoreCase(actionName)
+				|| PageAction.clickArticleImage.toString().equalsIgnoreCase(actionName)
+				|| PageAction.savedarticles.toString().equalsIgnoreCase(actionName)
+				|| PageAction.GoPremium.toString().equalsIgnoreCase(actionName)
+				|| PageAction.PicImage.toString().equalsIgnoreCase(actionName)
+				|| PageAction.opinions.toString().equalsIgnoreCase(actionName)
+				|| PageAction.subscription.toString().equalsIgnoreCase(actionName)
+				|| PageAction.savednewsarticle.toString().equalsIgnoreCase(actionName)
+				|| PageAction.MyActivity.toString().equalsIgnoreCase(actionName)
+				|| PageAction.Champs.toString().equalsIgnoreCase(actionName)
+				|| PageAction.Menu.toString().equalsIgnoreCase(actionName)
+				|| PageAction.HelpDesk.toString().equalsIgnoreCase(actionName)
+				|| PageAction.Month.toString().equalsIgnoreCase(actionName)
+				|| PageAction.Year.toString().equalsIgnoreCase(actionName)
+				|| PageAction.Cancel.toString().equalsIgnoreCase(actionName)
+				|| PageAction.MyProfile.toString().equalsIgnoreCase(actionName)
+				|| PageAction.FilterSavedArticles.toString().equalsIgnoreCase(actionName)
+				|| PageAction.ClearFilterSavedArticles.toString().equalsIgnoreCase(actionName)
+				|| PageAction.Scores.toString().equalsIgnoreCase(actionName)
+				|| PageAction.Trends.toString().equalsIgnoreCase(actionName)
+				|| PageAction.recognitions.toString().equalsIgnoreCase(actionName)
+				|| PageAction.LeftSwipe.toString().equalsIgnoreCase(actionName)
+				|| PageAction.RightSwipe.toString().equalsIgnoreCase(actionName)
+				|| PageAction.schooldetails.toString().equalsIgnoreCase(actionName)
+				|| PageAction.shareachievements.toString().equalsIgnoreCase(actionName)
+				|| PageAction.WelcomeUser.toString().equalsIgnoreCase(actionName)
+				|| PageAction.ClickNewsEvent.toString().equalsIgnoreCase(actionName)
+				|| PageAction.ClickOpinionArticle.toString().equalsIgnoreCase(actionName)) {
+			nextPageName = pageNavDto.getNextpage();
+		} else if (PageAction.previous.toString().equalsIgnoreCase(actionName)
+				|| PageAction.back.toString().equalsIgnoreCase(actionName)) {
 			nextPageName = pageNavDto.getPreviousPage();
 
-		}
-		else 
-		{
+		} else {
 			nextPageName = pageName;
 		}
-		
-		if(!pageName.equals(nextPageName)) {
+
+		if (!pageName.equals(nextPageName)) {
 			PageNavigationContext pageNavigationContext = new PageNavigationContext();
 			pageNavigationContext.setActionName(actionName);
 			pageNavigationContext.setPageRequest(pageRequest);
 			pageNavigationContext.setPreviousPageResponse(pageResponse);
 			pageNavigationContext.setPreviousPage(nextPageName);
-			
-		
-			//load data for the next page
-			pageResponse = pageHandlerFactory.getPageHandler(nextPageName,context).loadPage(pageNavigationContext);
-		}
-		else
-		{
+
+			// load data for the next page
+			pageResponse = pageHandlerFactory.getPageHandler(nextPageName, context).loadPage(pageNavigationContext);
+		} else {
 			PageNavigationContext pageNavigationContext = new PageNavigationContext();
 			pageNavigationContext.setActionName(actionName);
 			pageNavigationContext.setPageRequest(pageRequest);
 			pageNavigationContext.setPreviousPageResponse(pageResponse);
 			pageNavigationContext.setPreviousPage(pageName);
-			//load data for the same page
-			pageResponse = pageHandlerFactory.getPageHandler(pageName,context).loadPage(pageNavigationContext);
+			// load data for the same page
+			pageResponse = pageHandlerFactory.getPageHandler(pageName, context).loadPage(pageNavigationContext);
 		}
-		
-		addSuccessHeader(pageName, actionName, operation,pageResponse);
-		
+
+		addSuccessHeader(pageName, actionName, operation, pageResponse);
+
 		// attach UI controls for the next page to the response
-		addUIControls(nextPageName,pageResponse);
+		addUIControls(nextPageName, pageResponse);
 		return pageResponse;
 	}
-	
+
 	private void addSuccessHeader(String currentPageName, String actionName, String operation, PageDTO page) {
 
-		if(page.getHeader() == null) {
+		if (page.getHeader() == null) {
 			page.setHeader(new HeaderDTO());
 		}
 		page.getHeader().setRequestStatus(RequestStatusType.S);
 		page.getHeader().setPageName(page.getPageName());
-		
-		//String nextPageName = appConfig.getPageNavigationConfig().get(currentPageName.toLowerCase()).get(actionName.toLowerCase());
+
+		// String nextPageName =
+		// appConfig.getPageNavigationConfig().get(currentPageName.toLowerCase()).get(actionName.toLowerCase());
 		PageNavigatorDTO pageNavDto = pageNavigationService.getNavPage(actionName, operation, currentPageName);
 		String nextPageName = pageNavDto.getNextpage();
 		page.getHeader().setPageName(nextPageName);
 	}
-	
-	private void addUIControls(String pageName,PageDTO page )
-	{
-		List<UIControlsDTO> uiControls= uiControlService.get(pageName);
+
+	private void addUIControls(String pageName, PageDTO page) {
+		List<UIControlsDTO> uiControls = uiControlService.get(pageName);
 		page.setScreenProperties(uiControls);
 	}
 
-	
 }
-	
