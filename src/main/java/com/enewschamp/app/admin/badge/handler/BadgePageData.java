8afcbffe0957965d9715d3e0afa658709ebd87f1
sukhendra <sukhendra.parouha@db.com>
2021-04-10 19:06:35 +0530

    Added save and update image/audio for badge

--- src/main/java/com/enewschamp/app/admin/badge/handler/BadgePageData.java
+++ src/main/java/com/enewschamp/app/admin/badge/handler/BadgePageData.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotNull;
 import com.enewschamp.app.common.MessageConstants;
 import com.enewschamp.app.common.PageData;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
@@ -16,33 +17,48 @@ import lombok.EqualsAndHashCode;
 public class BadgePageData extends PageData {
 	private static final long serialVersionUID = 1L;
 
+	@JsonInclude
 	private Long badgeId;
 
 	@NotNull(message = MessageConstants.BADGE_NAME_NOT_NULL)
 	@NotEmpty(message = MessageConstants.BADGE_NAME_NOT_EMPTY)
 	private String nameId;
 
+	@JsonInclude
 	@NotNull(message = MessageConstants.GENRE_ID_NOT_NULL)
 	@NotEmpty(message = MessageConstants.GENRE_ID_NOT_EMPTY)
 	private String genreId;
+
 	private Long monthlyPointsToScore;
+
 	private String editionId;
 	@NotNull(message = MessageConstants.READING_LEVEL_NOT_NULL)
 	private int readingLevel;
-	private String imageName;
-	private String successImageName;
-	private String audioFileName;
+
 	private String base64Image;
-	private String imageTypeExt;
 
-	@JsonIgnore
-	public String getBase64Image() {
-		return this.base64Image;
-	}
+	private String base64SuccessImage;
 
-	@JsonProperty
-	public void setBase64Image(String base64Image) {
-		this.base64Image = base64Image;
-	}
+	private String base64AudioFile;
 
+	private String imageTypeExt = "jpg";
+
+	private String successImageTypeExt = "jpg";
+
+	private String audioFileTypeExt = "mp4";
+
+	@JsonInclude
+	private String imageName;
+
+	private String imageUpdate;
+
+	@JsonInclude
+	private String successImageName;
+
+	private String successImageUpdate;
+
+	@JsonInclude
+	private String audioFileName;
+
+	private String audioFileUpdate;
 }
--- src/main/java/com/enewschamp/app/admin/badge/handler/BadgePageHandler.java
+++ src/main/java/com/enewschamp/app/admin/badge/handler/BadgePageHandler.java
@@ -12,6 +12,7 @@ import javax.validation.ValidatorFactory;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
@@ -103,16 +104,7 @@ public class BadgePageHandler implements IPageHandler {
 		PageDTO pageDto = new PageDTO();
 		BadgePageData pageData = objectMapper.readValue(pageRequest.getData().toString(), BadgePageData.class);
 		validate(pageData);
-		Badge badge = mapBadgeData(pageRequest, pageData);
-		badge = badgeService.create(badge);
-		String newImageName = badge.getBadgeId() + "_" + System.currentTimeMillis();
-		String imageType = pageData.getImageTypeExt();
-		boolean saveFlag = commonService.saveImages("Admin", "badge", imageType, pageData.getBase64Image(),
-				newImageName);
-		if (saveFlag) {
-			badge.setImageName(newImageName + "." + imageType);
-			badge = badgeService.update(badge);
-		}
+		Badge badge = saveImageAndAudio(pageData);
 		mapBadge(pageRequest, pageDto, badge);
 		return pageDto;
 	}
@@ -128,8 +120,7 @@ public class BadgePageHandler implements IPageHandler {
 		PageDTO pageDto = new PageDTO();
 		BadgePageData pageData = objectMapper.readValue(pageRequest.getData().toString(), BadgePageData.class);
 		validate(pageData);
-		Badge badge = mapBadgeData(pageRequest, pageData);
-		badge = badgeService.update(badge);
+		Badge badge = updateImageAndAudio(pageData, pageData.getBadgeId());
 		mapBadge(pageRequest, pageDto, badge);
 		return pageDto;
 	}
@@ -223,5 +214,148 @@ public class BadgePageHandler implements IPageHandler {
 			});
 			throw new BusinessException(ErrorCodeConstants.INVALID_REQUEST);
 		}
+	}
+
+	private Badge updateImageAndAudio(BadgePageData badgeDTO, Long badgeId) {
+		badgeDTO.setBadgeId(badgeId);
+		Badge badge = badgeService.get(badgeId);
+		if (badge.getRecordInUse().equals(RecordInUseType.N)) {
+			throw new BusinessException(ErrorCodeConstants.RECORD_ALREADY_CLOSED);
+		}
+		String currentImageName = badge.getImageName();
+		String currentSuccessImageName = badge.getSuccessImageName();
+		String currentAudioFileName = badge.getAudioFileName();
+		badgeDTO.setAudioFileName(currentAudioFileName);
+		badgeDTO.setSuccessImageName(currentSuccessImageName);
+		badgeDTO.setImageName(currentImageName);
+		badge = modelMapper.map(badgeDTO, Badge.class);
+		badge = badgeService.update(badge);
+		boolean updateFlag = false;
+		if ("Y".equalsIgnoreCase(badgeDTO.getImageUpdate())) {
+			String newImageName = badge.getBadgeId() + "_IMG_" + System.currentTimeMillis();
+			String imageType = badgeDTO.getImageTypeExt();
+			boolean saveImageFlag = commonService.saveImages("Admin", "badge", imageType, badgeDTO.getBase64Image(),
+					newImageName);
+			if (saveImageFlag) {
+				badge.setImageName(newImageName + "." + imageType);
+				updateFlag = true;
+			} else {
+				badge.setImageName(null);
+				updateFlag = true;
+			}
+			if (currentImageName != null && !"".equals(currentImageName)) {
+				commonService.deleteImages("Admin", "badge", currentImageName);
+				updateFlag = true;
+			}
+		}
+		if ("Y".equalsIgnoreCase(badgeDTO.getSuccessImageUpdate())) {
+			String successImageType = badgeDTO.getSuccessImageTypeExt();
+			String newSucessImageName = badge.getBadgeId() + "_SI_" + System.currentTimeMillis();
+			boolean saveSuccessImageFlag = commonService.saveImages("Admin", "badge", successImageType,
+					badgeDTO.getBase64SuccessImage(), newSucessImageName);
+			if (saveSuccessImageFlag) {
+				badge.setSuccessImageName(newSucessImageName + "." + successImageType);
+				updateFlag = true;
+			} else {
+				badge.setSuccessImageName(null);
+				updateFlag = true;
+			}
+			if (currentSuccessImageName != null && !"".equals(currentSuccessImageName)) {
+				commonService.deleteImages("Admin", "badge", currentSuccessImageName);
+				updateFlag = true;
+			}
+		}
+		if ("Y".equalsIgnoreCase(badgeDTO.getAudioFileUpdate())) {
+			String audioFileName = badge.getBadgeId() + "_" + System.currentTimeMillis();
+			String audioFileType = badgeDTO.getAudioFileTypeExt();
+			boolean saveAudioFileFlag = commonService.saveAudioFile("Admin", "badge", audioFileType,
+					badgeDTO.getBase64AudioFile(), audioFileName);
+			if (saveAudioFileFlag) {
+				badge.setAudioFileName(audioFileName + "." + audioFileType);
+				updateFlag = true;
+			} else {
+				badge.setAudioFileName(null);
+				updateFlag = true;
+			}
+			if (currentAudioFileName != null && !"".equals(currentAudioFileName)) {
+				commonService.deleteAudios("Admin", "badge", currentAudioFileName);
+				updateFlag = true;
+			}
+		}
+		if (updateFlag) {
+			badge = badgeService.update(badge);
+		}
+		badge.setRecordInUse(RecordInUseType.Y);
+		return badge;
+	}
+
+	private Badge saveImageAndAudio(BadgePageData badgeDTO) {
+		Badge badge = modelMapper.map(badgeDTO, Badge.class);
+		try {
+			badge = badgeService.create(badge);
+		} catch (DataIntegrityViolationException e) {
+			log.error(e.getMessage());
+			throw new BusinessException(ErrorCodeConstants.RECORD_ALREADY_EXIST);
+		}
+		boolean updateFlag = false;
+		if ("Y".equalsIgnoreCase(badgeDTO.getImageUpdate())) {
+			String newImageName = badge.getBadgeId() + "_IMG_" + System.currentTimeMillis();
+			String imageType = badgeDTO.getImageTypeExt();
+			String currentImageName = badge.getImageName();
+			boolean saveImageFlag = commonService.saveImages("Admin", "badge", imageType, badgeDTO.getBase64Image(),
+					newImageName);
+			if (saveImageFlag) {
+				badge.setImageName(newImageName + "." + imageType);
+				updateFlag = true;
+			} else {
+				badge.setImageName(null);
+				updateFlag = true;
+			}
+			if (currentImageName != null && !"".equals(currentImageName)) {
+				commonService.deleteImages("Admin", "badge", currentImageName);
+				updateFlag = true;
+			}
+		}
+		if ("Y".equalsIgnoreCase(badgeDTO.getSuccessImageUpdate())) {
+			String successImageType = badgeDTO.getSuccessImageTypeExt();
+			String newSucessImageName = badge.getBadgeId() + "_SI_" + System.currentTimeMillis();
+			String currentSuccessImageName = badge.getSuccessImageName();
+			boolean saveSuccessImageFlag = commonService.saveImages("Admin", "badge", successImageType,
+					badgeDTO.getBase64SuccessImage(), newSucessImageName);
+			if (saveSuccessImageFlag) {
+				badge.setSuccessImageName(newSucessImageName + "." + successImageType);
+				updateFlag = true;
+			} else {
+				badge.setSuccessImageName(null);
+				updateFlag = true;
+			}
+			if (currentSuccessImageName != null && !"".equals(currentSuccessImageName)) {
+				commonService.deleteImages("Admin", "badge", currentSuccessImageName);
+				updateFlag = true;
+			}
+		}
+		if ("Y".equalsIgnoreCase(badgeDTO.getAudioFileUpdate())) {
+			String audioFileName = badge.getBadgeId() + "_" + System.currentTimeMillis();
+			String audioFileType = badgeDTO.getAudioFileTypeExt();
+			String currentAudioFileName = badge.getAudioFileName();
+			boolean saveAudioFileFlag = commonService.saveAudioFile("Admin", "badge", audioFileType,
+					badgeDTO.getBase64AudioFile(), audioFileName);
+			if (saveAudioFileFlag) {
+				badge.setAudioFileName(audioFileName + "." + audioFileType);
+				updateFlag = true;
+			} else {
+				badge.setAudioFileName(null);
+				updateFlag = true;
+			}
+			if (currentAudioFileName != null && !"".equals(currentAudioFileName)) {
+				commonService.deleteAudios("Admin", "badge", currentAudioFileName);
+				updateFlag = true;
+			}
+		}
+		if (updateFlag) {
+			badge = badgeService.update(badge);
+		}
+		badge.setRecordInUse(RecordInUseType.Y);
+		return badge;
 	}
 }
