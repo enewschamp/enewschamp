498bfd39a5c316afb26c8be14105b0321a72ada8
sukhendra <sukhendra.parouha@db.com>
2021-05-01 18:43:44 +0530

    removed unused

--- src/main/java/com/enewschamp/app/admin/otp/repository/OTPRepositoryCustomImpl.java
+++ src/main/java/com/enewschamp/app/admin/otp/repository/OTPRepositoryCustomImpl.java
@@ -1,13 +1,8 @@
 package com.enewschamp.app.admin.otp.repository;
 
-import java.security.Key;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -64,34 +59,5 @@ public class OTPRepositoryCustomImpl extends RepositoryImpl implements IGenericL
 		List<OTP> list = q.getResultList();
 		long count = getRecordCount(criteriaQuery, filterPredicates, otpRoot);
 		return new PageImpl<>(list, pageable, count);
-	}
-
-	private String encrypt(String value) {
-		Key key;
-		try {
-			key = new SecretKeySpec("3s6v9y$B&E)H@McQ".getBytes(), "AES");
-			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			byte[] iv = new byte[c.getBlockSize()];
-			c.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
-			return new String(Base64.getEncoder().encode(c.doFinal(value.getBytes()))).toString();
-//			System.out.println(">>>>Encrypted value>>>>>>"
-//					+ new String(Base64.getEncoder().encode(c.doFinal(value.getBytes()))).toString());
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	
-	public String  decrypt(String value) {
-		Key key;
-		try {
-			key = new SecretKeySpec("3s6v9y$B&E)H@McQ".getBytes(), "AES");
-			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			byte[] iv = new byte[c.getBlockSize()];
-			c.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
-			return new String(c.doFinal(Base64.getDecoder().decode(value)));
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
 	}
 }
